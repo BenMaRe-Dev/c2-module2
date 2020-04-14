7dc7fe73cd66c9abbaaa2db010fd824178190a48
BenMare-Dev <BenMaRe@Ben-MBP.local>
2020-04-14 09:28:18 -0500

    Solution improved

--- week-1/day-1/ideal-gas-law.rb
+++ week-1/day-1/ideal-gas-law.rb
@@ -1,8 +1,5 @@
-def calculate_pressure(chemical_amount, temperature, volume)
-  
- p = chemical_amount * 8.314 * temperature / volume 
-
- puts p
+def calculate_pressure(chemical_amount, temperature, volume)  
+ chemical_amount * 8.314 * temperature / volume 
 end
+puts calculate_pressure(5, 500, 2) # Returns 10392.5
 
-calculate_pressure(5, 500, 2) # Returns 10392.5
